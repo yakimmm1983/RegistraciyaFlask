5f3434288b68e8141f30fa14f7ff2d6479a5e8a8
yakim <yakimmm1983@yandex.ru>
2024-02-21 18:37:19 +0300

    variant

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask,render_template,request
-app = Flask("name")
+app = Flask("flaskapp")
 
-
-
-@app.route('/',methods = ['POST'])
+@app.route("")
 def registr():
+
+    return render_template("registrUser.html")
+
+@app.route("/user",metods=['POST'])
+def user():
     userName = request.form["userName"]
     surname = request.form["surname"]
     login = request.form["login"]
     password = request.form["password"]
-    return render_template('registrUser.html',userName=userName,surname=surname,
+    return render_template("user.html",userName=userName,surname=surname,
                            login=login,password=password)
 
 if __name__ == "__main__":
